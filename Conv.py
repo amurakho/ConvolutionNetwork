0f8176e797a32215b86ca54baf1fc0fb9a4eb285
amurakho <amurakho@gmail.com>
2019-03-29 14:12:11 +0200

    Create base skeleton of model

--- Conv.py
+++ Conv.py
@@ -5,30 +5,104 @@ It will be really simple, becose i make it only for training
 So this network will categorize... numbers, yeap
 again numbers(with ENG alphabet)
 
+Make hyperparams and skeleton from
+https://towardsdatascience.com/image-classification-in-10-minutes-with-mnist-dataset-54c35b77a38d
 """
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import tensorflow as tf
 
-def share_image():
+def share_image(train_images, test_images):
     """
     reshape image - make in same size(28x28)
-    separate to channels
+    simple normilize data
     """
-    pass
+    train_images = np.array(train_images)
+    test_images = np.array(test_images)
+
+    train_images = train_images.reshape(np.shape(train_images)[0], 28, 28)
+    test_images = test_images.reshape(np.shape(test_images)[0], 28, 28)
+
+    train_images = train_images / 255
+    test_images = test_images / 255
+
+    return(train_images, test_images)
+
 
 class Conv():
-    pass
+    """
+    Simple convolution model
+    predict:
+        make a prediction of image
+    Layer2D:
+        create convolution layer (2D)
+    """
+
+    def __init__(self):
+        pass
+
+    def predict(self, file_name=None):
+        """
+        make a prediction of image
+        :param file_name:
+            name of file with weight
+        :return:
+            label
+        """
+        pass
+
+    def Layer2D(self, data, kernel_size, strides=(1, 1), border_mode='valid'):
+        """
+        Create convolution layer
+        :param data:
+            image 28x28
+        :param kernel_size:
+            size of filter
+        :param strides:
+            filter "step"
+        :param border_mode:
+            mode which says how filter will work with map
+            if "valid":
+                take map and dont add some fields to it
+            if "full":
+                add to map some fields(with 0) for filter
+        :return:
+            new convolution map
+        """
+        if border_mode == 'valid':
+            pass
+        elif border_mode == 'full':
+            pass
+        else:
+            pass
+
+        pass
+
+    def MaxPooling2D(self):
+        pass
+
+    def Flatten(self):
+        pass
+
+    def Dropout(self):
+        pass
+
+    def Desnse(self):
+        pass
 
 
 if __name__ == '__main__':
 
-    df = pd.read_csv('data/train_images.csv')
+    train_images = pd.read_csv('data/train_images.csv')
+    test_images = pd.read_csv('data/test_images.csv')
 
-    test = np.array(df.iloc[0]).reshape(28, 28)
+    train_labels = pd.read_csv('data/train_labels.csv')
+    test_labels = pd.read_csv('data/test_labels.csv')
 
-    plt.imshow(test, cmap='Greys')
+    train_images, test_images = share_image(train_images, test_images)
 
-    plt.show()
+
+    # plt.imshow(test, cmap='Greys')
+    #
+    # plt.show()
--- input_data.py
+++ input_data.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 
 X, y = loadlocal_mnist(
-        images_path='data/train-images-idx3-ubyte',
-        labels_path='data/train-labels-idx1-ubyte')
+        images_path='data/t10k-images-idx3-ubyte',
+        labels_path='data/t10k-labels-idx1-ubyte')
 
 # print('Dimensions: %s x %s' % (X.shape[0], X.shape[1]))
 # print('\n1st row', X[0])
@@ -14,10 +14,10 @@ X, y = loadlocal_mnist(
 test = X[0].reshape(28, 28)
 
 print(np.shape(test))
-# np.savetxt(fname='data/train_images.csv',
-#            X=X, delimiter=',', fmt='%d')
-# np.savetxt(fname='data/train_labels.csv',
-#            X=y, delimiter=',', fmt='%d')
+np.savetxt(fname='data/test_images.csv',
+           X=X, delimiter=',', fmt='%d')
+np.savetxt(fname='data/test_labels.csv',
+           X=y, delimiter=',', fmt='%d')
 
 # df = pd.read_csv('data/train_images.csv')
 
